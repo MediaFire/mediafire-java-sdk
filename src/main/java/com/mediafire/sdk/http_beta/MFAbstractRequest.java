21a8a43b2d9f19aa750f5daf99351e6a9d9b5c7f
Christopher Najar <chris.najar@mediafire.com>
2014-10-01 15:24:05 -0500

    test

--- src/main/java/com/mediafire/sdk/http_beta/MFAbstractRequest.java
+++ src/main/java/com/mediafire/sdk/http_beta/MFAbstractRequest.java
@@ -4,5 +4,5 @@ package com.mediafire.sdk.http_beta;
  * Created by Chris Najar on 10/1/2014.
  */
 public abstract class MFAbstractRequest implements MFRequestInterface {
-
+    private String test;
 }

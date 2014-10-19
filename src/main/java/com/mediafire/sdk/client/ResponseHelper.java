6cd95f51131e6ce335497f4ef691b97416f3705b
Christopher Najar <chris.najar@mediafire.com>
2014-10-18 21:58:08 -0500

    STARS#26331
    changes from sdk

--- src/main/java/com/mediafire/sdk/client/ResponseHelper.java
+++ src/main/java/com/mediafire/sdk/client/ResponseHelper.java
@@ -1,13 +1,16 @@
 package com.mediafire.sdk.client;
 
 import com.mediafire.sdk.api_responses.ApiResponse;
+import com.mediafire.sdk.http.Response;
 
 /**
  * Created by Chris Najar on 10/18/2014.
  */
 public class ResponseHelper {
-    public ResponseHelper() {
+    private Response mResponse;
 
+    public ResponseHelper(Response response) {
+        mResponse = response;
     }
 
     public ApiResponse getApiResponse() {

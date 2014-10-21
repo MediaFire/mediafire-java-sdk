a9395b5c54207ab09f8ab025502689738fd1f496
Christopher Najar <chris.najar@mediafire.com>
2014-10-21 11:18:28 -0500

    STARS#26331
                        added base unit test

--- src/main/tests/com/mediafire/sdk/clients/ApiClientTest.java
+++ src/main/tests/com/mediafire/sdk/clients/ApiClientTest.java
@@ -1,16 +1,34 @@
 package com.mediafire.sdk.clients;
 
+import com.mediafire.sdk.config.Configuration;
+import com.mediafire.sdk.http.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertNotNull;
 
 /**
  * Created by Chris Najar on 10/21/2014.
  */
 public class ApiClientTest {
+    Configuration mConfiguration;
+
     @Before
     public void setUp() throws Exception {
+        mConfiguration = new Configuration.Builder().build();
 
+        Map<String, String> devCredentials = new LinkedHashMap<String, String>();
+        devCredentials.put("application_id", "18");
+        mConfiguration.getDeveloperCredentials().setCredentials(devCredentials);
+
+        Map<String, String> userCredentials = new LinkedHashMap<String, String>();
+        userCredentials.put("email", "javasdktest@example.com");
+        userCredentials.put("password", "74107410");
+        mConfiguration.getUserCredentials().setCredentials(userCredentials);
     }
 
     @After
@@ -19,29 +37,30 @@ public class ApiClientTest {
     }
 
     @Test
-    public void testDoRequest() throws Exception {
+    public void testSystemGetStatusGet() throws Exception {
+        ApiClient apiClient = new ApiClient(mConfiguration.getHttpWorker(), mConfiguration.getSessionTokenManager(), mConfiguration.getActionTokenManager(), mConfiguration.getUserCredentials(), mConfiguration.getDeveloperCredentials());
+        HostObject hostObject = new HostObject("http", "www", "mediafire.com", "get");
+        ApiObject apiObject = new ApiObject("system", "get_status.php");
+        InstructionsObject instructionsObject = new InstructionsObject(BorrowTokenType.NONE, SignatureType.NO_SIGNATURE_REQUIRED, ReturnTokenType.NONE, false);
+        VersionObject versionObject = new VersionObject("1.2");
+        Request request = new Request(hostObject, apiObject, instructionsObject, versionObject);
+        request.addQueryParameter("response_format", "json");
+        Result result = apiClient.doRequest(request);
 
+        assertNotNull(result);
     }
 
     @Test
-    public void testDoRequest1() throws Exception {
+    public void testSystemGetStatusPost() throws Exception {
+        ApiClient apiClient = new ApiClient(mConfiguration.getHttpWorker(), mConfiguration.getSessionTokenManager(), mConfiguration.getActionTokenManager(), mConfiguration.getUserCredentials(), mConfiguration.getDeveloperCredentials());
+        HostObject hostObject = new HostObject("http", "www", "mediafire.com", "post");
+        ApiObject apiObject = new ApiObject("system", "get_status.php");
+        InstructionsObject instructionsObject = new InstructionsObject(BorrowTokenType.NONE, SignatureType.NO_SIGNATURE_REQUIRED, ReturnTokenType.NONE, true);
+        VersionObject versionObject = new VersionObject("1.2");
+        Request request = new Request(hostObject, apiObject, instructionsObject, versionObject);
+        request.addQueryParameter("response_format", "json");
+        Result result = apiClient.doRequest(request);
 
+        assertNotNull(result);
     }
-
-    @Test
-    public void testDoRequest2() throws Exception {
-
-    }
-
-    @Test
-    public void testDoGet() throws Exception {
-
-    }
-
-    @Test
-    public void testDoPost() throws Exception {
-
-    }
-
-    
 }

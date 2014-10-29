9d1689896ac864eca8fa1d8d15590d0e2faa5c7c
Chris Najar <chris.najar@mediafire.com>
2014-10-29 16:44:01 -0500

    STARS#26728
    added base unit test.

---
+++ src/main/tests/com/mediafire/sdk/clients/meta/MetaClientTest.java
@@ -0,0 +1,71 @@
+package com.mediafire.sdk.clients.meta;
+
+import junit.framework.TestCase;
+
+public class MetaClientTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testAddToList() throws Exception {
+
+    }
+
+    public void testAddToList1() throws Exception {
+
+    }
+
+    public void testRemoveFromList() throws Exception {
+
+    }
+
+    public void testRemoveFromList1() throws Exception {
+
+    }
+
+    public void testDelete() throws Exception {
+
+    }
+
+    public void testDelete1() throws Exception {
+
+    }
+
+    public void testGet() throws Exception {
+
+    }
+
+    public void testGet1() throws Exception {
+
+    }
+
+    public void testGetLinks() throws Exception {
+
+    }
+
+    public void testGetLinks1() throws Exception {
+
+    }
+
+    public void testGetLinks2() throws Exception {
+
+    }
+
+    public void testGetLinks3() throws Exception {
+
+    }
+
+    public void testQuery() throws Exception {
+
+    }
+
+    public void testSet() throws Exception {
+
+    }
+}

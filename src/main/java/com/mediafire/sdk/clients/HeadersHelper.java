2b6aa972839a89d0c7994fc8b111079e49f18e1f
Jonathan Harrison <jonathan.harrison@mediafire.com>
2014-10-29 15:35:37 -0500

    NO STARS
    added documentation to the classes in the clients package

--- src/main/java/com/mediafire/sdk/clients/HeadersHelper.java
+++ src/main/java/com/mediafire/sdk/clients/HeadersHelper.java
@@ -5,21 +5,33 @@ import com.mediafire.sdk.http.Request;
 
 /**
  * Created by Chris Najar on 10/19/2014.
+ * HeadersHelper adds appropriate headers for a Request object
  */
 public class HeadersHelper {
     private static final String TAG = HeadersHelper.class.getCanonicalName();
     private final Request mRequest;
     private final String CHARSET = "UTF-8";
 
+    /**
+     * HeadersHelper Constructor
+     * @param request the Request to add the headers to
+     */
     public HeadersHelper(Request request) {
         mRequest = request;
     }
 
+    /**
+     * Adds headers for a get http request
+     */
     public void addGetHeaders() {
         DefaultLogger.log().v(TAG, "addGetHeaders");
         mRequest.addHeader("Accept-Charset", CHARSET);
     }
 
+    /**
+     * Adds headers for a post http request based on the the requests' query is to be posted
+     * @param payload the payload to be sent with the request
+     */
     public void addPostHeaders(byte[] payload) {
         DefaultLogger.log().v(TAG, "addPostHeaders");
         mRequest.addHeader("Accept-Charset", CHARSET);

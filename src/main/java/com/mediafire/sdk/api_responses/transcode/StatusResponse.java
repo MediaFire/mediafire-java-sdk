34be06748a285d1331bd6139d0f5ad29d93c8cc1
Jonathan Harrison <jonathan.harrison@mediafire.com>
2014-11-04 16:23:33 -0600

    STARS #26743
    Added responses for Transcode responses

---
+++ src/main/java/com/mediafire/sdk/api_responses/transcode/StatusResponse.java
@@ -0,0 +1,104 @@
+package com.mediafire.sdk.api_responses.transcode;
+
+import com.mediafire.sdk.http.Response;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * Created by jondh on 11/4/14.
+ */
+public class StatusResponse {
+    private static final String NAME_TFTP_PROGRESS = "tftp_progress";
+    private static final String NAME_VIDEO_WIDTH = "video_width";
+    private static final String NAME_VIDEO_HEIGHT = "video_height";
+    private static final String NAME_VIDEO_BITRATE = "video_bitrate";
+    private static final String NAME_AUDIO_BITRATE = "audio_bitrate";
+    private static final String NAME_DURATION = "duration";
+    private static final String NAME_ORIGINAL_FILESIZE = "original_filesize";
+    private static final String NAME_TRANSCODED_FILESIZE = "transcoded_filesize";
+    private static final String NAME_STATUS = "status";
+    private static final String NAME_SCHEMA_VERSION = "schema_version";
+
+    public String tftp_progress = "0";
+    public int video_width = 0;
+    public int video_height = 0;
+    public long video_bitrate = 0;
+    public long audio_bitrate = 0;
+    public int duration = 0;
+    public long original_filesize = 0;
+    public long transcoded_filesize = 0;
+    public String status = "";
+    public int schema_version = 0;
+
+    private Response mResponse;
+
+    public StatusResponse(Response response) {
+        mResponse = response;
+
+        parseXml();
+    }
+
+    public void parseXml() {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+
+            Document document = documentBuilder.parse(new ByteArrayInputStream(mResponse.getBytes()));
+
+            NodeList nodeList = document.getDocumentElement().getChildNodes();
+
+            for(int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                String nodeName = node.getNodeName().trim();
+
+                if(nodeName.equals(NAME_TFTP_PROGRESS)) {
+                    tftp_progress = node.getTextContent();
+                }
+                else if(nodeName.equals(NAME_VIDEO_WIDTH)) {
+                    video_width = Integer.parseInt(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_VIDEO_HEIGHT)) {
+                    video_height = Integer.parseInt(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_VIDEO_BITRATE)) {
+                    video_bitrate = Long.parseLong(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_AUDIO_BITRATE)) {
+                    audio_bitrate = Long.parseLong(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_DURATION)) {
+                    duration = Integer.parseInt(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_ORIGINAL_FILESIZE)) {
+                    original_filesize = Long.parseLong(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_TRANSCODED_FILESIZE)) {
+                    transcoded_filesize = Long.parseLong(node.getTextContent());
+                }
+                else if(nodeName.equals(NAME_STATUS)) {
+                    status = node.getTextContent();
+                }
+                else if(nodeName.equals(NAME_SCHEMA_VERSION)) {
+                    schema_version = Integer.parseInt(node.getTextContent());
+                }
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

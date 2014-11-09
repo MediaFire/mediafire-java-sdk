5f9da36f0d35eb4374267593c4b354d259b85685
Christopher Najar <chris.najar@mediafire.com>
2014-11-09 12:42:58 -0600

    26771
    added ApiVersion class. namespace for versions

---
+++ src/main/java/com/mediafire/sdk/http/ApiVersion.java
@@ -0,0 +1,16 @@
+package com.mediafire.sdk.http;
+
+/**
+ * Created by Chris on 11/9/2014.
+ */
+public class ApiVersion {
+    private ApiVersion() { }
+
+    public static final String LEGACY = null;
+    public static final String VERSION_1_0 = "1.0";
+    public static final String VERSION_1_1 = "1.1";
+    // current version as of 11/9/2014
+    public static final String VERSION_CURRENT = "1.2";
+    // current development version as of 11/9/2014
+    public static final String VERSION_DEVELOPMENT = "1.3";
+}

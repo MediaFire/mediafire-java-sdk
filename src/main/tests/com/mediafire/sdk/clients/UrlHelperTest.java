8dedce2490e86b26dece625dca36ea38b6374441
Christopher Najar <chris.najar@mediafire.com>
2014-11-10 14:32:24 -0600

    26771
    unit test skeleton for UrlHelper

---
+++ src/main/tests/com/mediafire/sdk/clients/UrlHelperTest.java
@@ -0,0 +1,39 @@
+package com.mediafire.sdk.clients;
+
+import junit.framework.TestCase;
+
+public class UrlHelperTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetUrlForPOSTRequest() throws Exception {
+
+    }
+
+    public void testGetUrlForGETRequest() throws Exception {
+
+    }
+
+    public void testGetBaseUrlString() throws Exception {
+
+    }
+
+    public void testGetBaseUriString() throws Exception {
+
+    }
+
+    public void testGetQueryString() throws Exception {
+
+    }
+
+    public void testGetQueryString1() throws Exception {
+
+    }
+}

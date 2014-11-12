1811a9d43494ece3c9ce73346165bf1a2ef80c84
Christopher Najar <chris.najar@mediafire.com>
2014-11-12 15:06:57 -0600

    26771
    removed xml response classes. gson style classes added.

---
+++ src/main/java/com/mediafire/sdk/transcode/responses/CreateResponse.java
@@ -0,0 +1,17 @@
+package com.mediafire.sdk.transcode.responses;
+
+/**
+ * Created by Chris on 11/12/2014.
+ */
+public class CreateResponse {
+    private String state;
+    private String duration;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+}
--- src/main/java/com/mediafire/sdk/transcode/responses/ParseTypeXml.java
+++
@@ -1,8 +0,0 @@
-package com.mediafire.sdk.transcode.responses;
-
-/**
- * Created by jondh on 11/5/14.
- */
-public enum ParseTypeXml {
-    XML, JSON, NONE
-}

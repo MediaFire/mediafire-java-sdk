7cd5f0a60093d0c7cebb5f9f2ef4239f797e08cc
Christopher Najar <chris.najar@mediafire.com>
2014-11-18 14:10:35 -0600

    STARS#26772
    corrected QueryResponse object format

--- src/main/java/com/mediafire/sdk/api/responses/meta/QueryResponse.java
+++ src/main/java/com/mediafire/sdk/api/responses/meta/QueryResponse.java
@@ -9,121 +9,110 @@ import java.util.List;
  * Created by Chris Najar on 10/2/2014.
  */
 public class QueryResponse extends ApiResponse {
-    private Items items;
+    List<Item> item;
 
-    public Items getItems() {
-        if (items == null) {
-            items = new Items();
+    public List<Item> getItemsList() {
+        if (item == null) {
+            item = new LinkedList<Item>();
         }
-        return items;
+        return item;
     }
 
-    public class Items {
-        List<Item> item;
+    public class Item {
+        private String quickkey;
+        private String filename;
+        private String created;
+        private String size;
+        private String mimetype;
+        private String media_type;
+        private String manufacturer;
+        private String model;
+        private String orientation;
+        private String software;
+        private String date_and_time;
+        private String compression;
+        private String x_resolution;
+        private String y_resolution;
+        private String width;
+        private String height;
 
-        public List<Item> getItemsList() {
-            if (item == null) {
-                item = new LinkedList<Item>();
-            }
-            return item;
+        public String getQuickkey() {
+            return quickkey;
         }
 
-        public class Item {
-            private String quickkey;
-            private String filename;
-            private String created;
-            private String size;
-            private String mimetype;
-            private String media_type;
-            private String manufacturer;
-            private String model;
-            private String orientation;
-            private String software;
-            private String date_and_time;
-            private String compression;
-            private String x_resolution;
-            private String y_resolution;
-            private String width;
-            private String height;
+        public String getFileName() {
+            return filename;
+        }
 
-            public String getQuickkey() {
-                return quickkey;
-            }
+        public String getCreated() {
+            return created;
+        }
 
-            public String getFileName() {
-                return filename;
+        public long getSize() {
+            if (size == null) {
+                size = "0";
             }
+            return Long.parseLong(size);
+        }
 
-            public String getCreated() {
-                return created;
-            }
+        public String getMimeType() {
+            return mimetype;
+        }
 
-            public long getSize() {
-                if (size == null) {
-                    size = "0";
-                }
-                return Long.parseLong(size);
-            }
+        public String getMediaType() {
+            return media_type;
+        }
 
-            public String getMimeType() {
-                return mimetype;
-            }
+        public String getManufacturer() {
+            return manufacturer;
+        }
 
-            public String getMediaType() {
-                return media_type;
-            }
+        public String getModel() {
+            return model;
+        }
 
-            public String getManufacturer() {
-                return manufacturer;
-            }
+        public String getOrientation() {
+            return orientation;
+        }
 
-            public String getModel() {
-                return model;
-            }
+        public String getSoftware() {
+            return software;
+        }
 
-            public String getOrientation() {
-                return orientation;
-            }
+        public String getDateAndTime() {
+            return date_and_time;
+        }
 
-            public String getSoftware() {
-                return software;
-            }
+        public String getCompression() {
+            return compression;
+        }
 
-            public String getDateAndTime() {
-                return date_and_time;
+        public int getXResolution() {
+            if (x_resolution == null) {
+                x_resolution = "0";
             }
+            return Integer.parseInt(x_resolution);
+        }
 
-            public String getCompression() {
-                return compression;
+        public int getYResolution() {
+            if (y_resolution == null) {
+                y_resolution = "0";
             }
+            return Integer.parseInt(y_resolution);
+        }
 
-            public int getXResolution() {
-                if (x_resolution == null) {
-                    x_resolution = "0";
-                }
-                return Integer.parseInt(x_resolution);
+        public int getWidth() {
+            if (width == null) {
+                width = "0";
             }
+            return Integer.parseInt(width);
+        }
 
-            public int getYResolution() {
-                if (y_resolution == null) {
-                    y_resolution = "0";
-                }
-                return Integer.parseInt(y_resolution);
+        public int getHeight() {
+            if (height == null) {
+                height = "0";
             }
-
-            public int getWidth() {
-                if (width == null) {
-                    width = "0";
-                }
-                return Integer.parseInt(width);
-            }
-
-            public int getHeight() {
-                if (height == null) {
-                    height = "0";
-                }
-                return Integer.parseInt(height);
-            }
+            return Integer.parseInt(height);
         }
     }
 }
